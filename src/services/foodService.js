5cf200477d80b9140bd33159f62fc8d80a630a48
sandipnale <sandipnale143sa@gmail.com>
2025-08-04 08:00:50 +0530

    Update foodService.js

--- src/services/foodService.js
+++ src/services/foodService.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API_URl = 'http://localhost:8080/api/foods';
+const API_URl = 'https://bedrooms-monsters-ties-ntsc.trycloudflare.com/api/foods';
 
 export const addFood = async (foodData, image) => {
 
